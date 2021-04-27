5539d0ece41aa4d2bb8e4560a4c2f00bacbc7110
Brokeassviking <denrose92@gmail.com>
2021-04-27 21:50:08 +0200

    did stuff

--- js/funfact.js
+++ js/funfact.js
@@ -1,2 +1,13 @@
 
-console.log('i think this is hard af')
+console.log('i think this is hard af');
+console.log("but i do not give up")
+
+setTimeout(delaytxt() => {
+    
+}, 2000);
+function delaytxt(){
+    
+
+    document.getElementsByName("delaytxt").style.visebility = "visible"
+}
+
