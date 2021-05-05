8b5660a467b370f028597f4cd19a2a06ca0760a9
Brokeassviking <denrose92@gmail.com>
2021-05-05 20:45:34 +0200

    finished robots and meds

--- js/funfact.js
+++ js/funfact.js
@@ -2,8 +2,29 @@
 console.log('i think this is hard af');
 console.log("but i do not give up")
 
-const target= document.querySelector(".delaytxt")
-const span= document.querySelector("fun-fact") 
+const button = document.querySelector(".fun-fact");
+const fact = document.querySelector(".fact");
+
+let timer;
+function setTime() {
+  timer = setTimeout(() => {
+    fact.style.display = "block";
+  }, 1000);
+}
+
+function clearTime() {
+  clearTimeout(timer);
+}
+button.onmouseover = () => {
+  button.classList.add("over");
+  setTime();
+};
+
+onmouseout = function () {
+  button.classList.remove("over");
+  clearTime();
+  fact.style.display = "none";
+};
 
 
 
